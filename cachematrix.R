feeb7e435ef8bd48597b33bad611dfdae49143a1
das44 <das44@njit.edu>
2015-06-21 01:32:22 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,12 @@
-## These functions can be used to compute the inverse
-## of a 2x2 matrix. The value of the most recent 
-##computed matrix is stored and retreived if the 
+## These functions can be used to compute and cache
+## the inverse of a matrix. The value of the most recent 
+## computed inverse is stored and retreived if the 
 ## function is called again for the same matrix
 
 ##It is assumed that the matrix is invertible
 
-## The makeCacheMatrix 
+## The makeCacheMatrix must be run first. The only input
+## is the matrix to be inverted
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- matrix(nrow = 2, ncol = 2) ## Initialize inverse matrix locally
@@ -18,9 +19,9 @@ makeCacheMatrix <- function(x = matrix()) {
     inverse <<- matrix(nrow = 2, ncol = 2)
   }
   get <- function() x ## Retrieve the matrx that was inverted
-  setinv <- function(inv) inverse <<- inv
+  setinv <- function(inv) inverse <<- inv ## Set the cached inverse
   
-  getinv <- function() inverse
+  getinv <- function() inverse ## Retrieve the cached inverse
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
@@ -28,18 +29,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Run this function to compute the matrix inverse 
+## or retrieve if previously computed
 
 cacheSolve <- function(x, ...) {
+  ## Check if the inverse has been previously computed
   if (!identical(x$getinv(), matrix(nrow = 2, ncol = 2))){
     message("Retreiving from cache")
     x$getinv() 
   }
   else {
-    newMatrix <- x$get()
-    x$set(newMatrix)
-    x$setinv(solve(newMatrix,...))
     message("Computing inverse")
+    x$setinv(solve(x$get(), ...))
     x$getinv()
   }
   
